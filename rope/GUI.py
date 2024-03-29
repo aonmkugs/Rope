5034ad2c125705b42f0637aacd38c410055c0273
aonmkugs <email@example.com>
2024-03-29 21:13:15 +0100

    feat: support utf8 file paths

--- rope/GUI.py
+++ rope/GUI.py
@@ -965,10 +965,11 @@ class GUI(tk.Tk):
                 print('Unrecognized file type:', file)
             else:
                 # Its an image
-                if file_type == 'image':
-                    img = cv2.imread(file)
-
-                    if img is not None:
+                if file_type == 'image':                
+                    # Decode the byte array into an OpenCV image
+                    img = cv2.imdecode(np.fromfile(file, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+                    
+                    if img is not None:     
                         img = torch.from_numpy(img.astype('uint8')).to('cuda')
 
                         pad_scale = 0.2
@@ -1213,7 +1214,7 @@ class GUI(tk.Tk):
                 # Its an image
                 if file_type == 'image':
                     try:
-                        image = cv2.imread(file)
+                        image = cv2.imdecode(np.fromfile(file, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
                         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                     except:
                         print('Trouble reading file:', file)
